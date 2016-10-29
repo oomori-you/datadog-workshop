e8c92a7e4306d19c0e8de2b36dd59427670025cb
oomori-you <valsamina@gmail.com>
2016-10-29 19:14:55 +0900

    composer経由でphp-datadogstatsdを入れるにしてもgitが必要だった

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -eu
 
-# apache
 apt-get update
+
+# git
+apt-get -y install git
+
+# apache
 apt-get -y install apache2
 
 # ab
-apt-get update
 apt-get -y install apache2-utils
 
 # php
@@ -18,9 +21,7 @@ apt-get -y install php7.0
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 # pip
-apt-get update
 apt-get -y install python-pip
 
 # fabric
-apt-get update
 apt-get -y install fabric
